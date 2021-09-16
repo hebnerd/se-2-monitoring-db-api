d51319689b66a5e067c234f3363996f0bc15f80c
Aaron Henderson <HENDERSONAW@ETSU.EDU>
2021-09-16 15:58:08 -0400

    Created GET, POST, PATCH, and DELETE functions for Registered_User table in DB.

--- db/monitoring.js
+++ db/monitoring.js
@@ -1,7 +1,33 @@
 const knex = require('./knex');
 
+// CREATE functions
+function createRegisteredUser(user) {
+	return knex('Users_Registered').insert(user);
+}
+
+// READ functions
 function getAllRegisteredUsers(){
 	return knex('Users_Registered').select('*');
 }
 
-module.exports = { getAllRegisteredUsers }
+function getRegisteredUser(id) {
+	return knex('Users_Registered').where('USER_ID', id);
+}
+
+// UPDATE functions
+function updateRegisteredUser(id, user) {
+	return knex('Users_Registered').where('USER_ID', id).update(user);
+}
+
+// DELETE functions
+function deleteRegisteredUser(id) {
+	return knex('Users_Registered').where('USER_ID', id).del();
+}
+
+module.exports = {
+	createRegisteredUser,
+	getAllRegisteredUsers,
+	getRegisteredUser,
+	updateRegisteredUser,
+	deleteRegisteredUser
+}
--- server.js
+++ server.js
@@ -8,6 +8,26 @@ fastify.get(URL_Prefix + '/users/registered', async(req, res) => {
 	res.code(200).send({ results });
 });
 
+fastify.get(URL_Prefix + '/users/registered/:id', async(req, res) => {
+	const results = await db.getRegisteredUser(req.params.id);
+	res.code(200).send({ results });
+});
+
+fastify.post(URL_Prefix + '/users/registered', async (req, res) => {
+	const results = await db.createRegisteredUser(JSON.parse(req.body));
+	res.code(201).send({ id: results });
+});
+
+fastify.patch(URL_Prefix + '/users/registered/:id', async (req, res) => {
+	const results = await db.updateRegisteredUser(req.params.id, JSON.parse(req.body));
+	res.code(200).send({ id: results });
+});
+
+fastify.delete(URL_Prefix + '/users/registered/:id', async (req, res) => {
+	const results = await db.deleteRegisteredUser(req.params.id);
+	res.code(200).send({ success: true });
+});
+
 fastify.listen(8000, function(err, address){
 	if(err){
 		console.log(err);
