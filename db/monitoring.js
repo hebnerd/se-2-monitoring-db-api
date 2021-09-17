2a98d921d05e943981dbf9a2df7157fe4c4d3219
Josh <kingjc@etsu.edu>
2021-09-17 10:59:25 -0400

    Added the Rest of the CRUD Actions to Monitoring.js

--- db/monitoring.js
+++ db/monitoring.js
@@ -1,5 +1,6 @@
 const knex = require('./knex');
 
+// RegisteredUsers CRUD
 // CREATE functions
 function createRegisteredUser(user) {
 	return knex('Users_Registered').insert(user);
@@ -24,10 +25,99 @@ function deleteRegisteredUser(id) {
 	return knex('Users_Registered').where('USER_ID', id).del();
 }
 
+// OnlineUsers CRUD
+// CREATE functions
+function createOnlineUsers() {
+	return knew('Users_Online').insert(user);
+}
+
+// READ functions
+function getAllOnlineUsers() {
+	return knex('Users_Online').select('*');
+}
+
+function getOnlineUsers(id) {
+	return knew('Users_Online').where('ONLINE_ID', id);
+}
+
+// UPDATE functions
+function updateOnlineUsers(id, user) {
+	return knex('Users_Online').where('ONLINE_ID', id).update(user);
+}
+
+// DELETE functions
+function deleteOnlineUsers(id) {
+	return knew('Users_Online').where('ONLINE_ID', id).del();
+}
+
+// PagesViewed CRUD
+// CREATE functions
+function createPagesViewed() {
+	return knew('Pages_Viewed').insert(user);
+}
+
+// READ functions
+function getAllPagesViewed() {
+	return knew('Pages_Viewed').select('*');
+}
+
+function getPagesViewed(id) {
+	return knew('Pages_Viewed').where('PAGE_ID', id);
+}
+
+// UPDATE functions
+function updatePagesViewed(id, user) {
+	return knew('Pages_Viewed').where('PAGE_ID', id).update(user);
+}
+
+// DELETE functions
+function deletePagesViewed(id) {
+	return knew('Pages_Viewed').where('PAGE_ID', id).del();
+}
+// Session CRUD
+// CREATE functions
+function createSession() {
+	return knew('Session').insert(user);
+}
+
+// READ functions
+function getAllSession() {
+	return knew("Session").select('*');
+}
+
+function getSession(id) {
+	return knew('Session').where('SESSION_ID', id);
+}
+
+// UPDATE functions
+function updateSession(id, user) {
+	return knew('Session').where('SESSION_ID', id).update(user);
+}
+
+// DELETE functions
+function deleteSession(id) {
+	return knew('Session').where('SESSION_ID', id).del();
+}
+
 module.exports = {
 	createRegisteredUser,
 	getAllRegisteredUsers,
 	getRegisteredUser,
 	updateRegisteredUser,
-	deleteRegisteredUser
+	deleteRegisteredUser,
+	createOnlineUsers,
+	getAllOnlineUsers,
+	getOnlineUsers,
+	updateOnlineUsers,
+	deleteOnlineUsers,
+	createPagesViewed,
+	getAllPagesViewed,
+	getPagesViewed,
+	updatePagesViewed,
+	deletePagesViewed,
+	createSession,
+	getAllSession,
+	getSession,
+	updateSession,
+	deleteSession
 }
