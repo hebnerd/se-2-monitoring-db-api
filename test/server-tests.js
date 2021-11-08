f6b38cb3eff525f9a027e7218ae4548a7943c20d
Destin Hebner <hebnerd@etsu.edu>
2021-11-08 13:30:38 -0500

    updated server.js tests

--- test/server-tests.js
+++ test/server-tests.js
@@ -6,19 +6,21 @@ let should = chai.should();             // more assertion stuff
 
 chai.use(chaiHttp);                     // tells chai we'll be using http stuff
 
-describe('/GET views', () =>            // name of test group
+let server = require("../server");
+
+describe('server.js', () =>            // name of test group
 {
     // name of test
-    it('it should GET view stats', (done) =>
+    it('/ping', (done) =>
     {
-        chai.request("localhost:8000")                  // on this server
-            .get('/api/monitoring/usage/views/1')       // get this route
+        chai.request("localhost:8000")                              // on this server
+            .get('/api/monitoring/ping')                            // get this route
             .end((err, res) => 
             {
-                res.should.have.status(200);        // 200 (ok) status code
-                res.body.should.be.an('object');    // data should be an object
+                res.should.have.status(200);                        // 200 (ok) status code
+                res.body.should.have.property('Status', 'Running'); // expected JSON
 
-                done();                             // done with test
+                done();                                             // done with test
             });
     });
 });
